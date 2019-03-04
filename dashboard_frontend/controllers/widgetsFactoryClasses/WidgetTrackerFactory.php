48ec2c3bf247fbb7fa6dff84257de8bc0eab1f9f
KindofBlue79 <kindofblue79@gmail.com>
2019-03-04 13:00:55 +0100

    commit_4_marzo_2019

--- dashboard_frontend/controllers/widgetsFactoryClasses/WidgetTrackerFactory.php
+++ dashboard_frontend/controllers/widgetsFactoryClasses/WidgetTrackerFactory.php
@@ -19,7 +19,15 @@ class WidgetTrackerFactory extends aGenericWidgetFactory
         $this->startParams->parameters = json_encode($widgetParameters);
         $this->startParams->id_metric = "Tracker";
         $this->startParams->name_w = str_replace("ToBeReplacedByFactory", "Tracker", $this->startParams->name_w);
-        $this->startParams->sm_based = "myPersonalData";
+        if ($selectedRow['high_level_type'] === "My Personal Data") {
+            $this->startParams->sm_based = "myPersonalData";
+        } else if ($selectedRow['high_level_type'] === "MyKPI") {
+            $this->startParams->sm_based = "myKPI";
+            $this->startParams->rowParameters = $selectedRow['parameters'];
+        } else if ($selectedRow['high_level_type'] === "MyData") {
+            $this->startParams->sm_based = "myData";
+            $this->startParams->rowParameters = $selectedRow['parameters'];
+        }
         $this->startParams->title_w = "Tracker";
         return $this->startParams;
     }
