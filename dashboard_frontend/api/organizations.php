e7bae6c4f91d3bf87f696efba195eb39290de08f
KindofBlue79 <kindofblue79@gmail.com>
2019-04-01 10:28:08 +0200

    allowed CORS

--- dashboard_frontend/api/organizations.php
+++ dashboard_frontend/api/organizations.php
@@ -15,6 +15,7 @@
    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA. */
 
   include '../config.php';
+  header('Access-Control-Allow-Origin: *');
 
   $link = mysqli_connect($host, $username, $password);
   mysqli_select_db($link, $dbname);
