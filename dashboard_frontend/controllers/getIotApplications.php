4d08d7cd721e1d6486260c72a8294a0fce1a1c06
KindofBlue79 <kindofblue79@gmail.com>
2019-04-23 11:50:40 +0200

    23 Aprile 2019

--- dashboard_frontend/controllers/getIotApplications.php
+++ dashboard_frontend/controllers/getIotApplications.php
@@ -37,7 +37,51 @@ if (isset($_SESSION['refreshToken'])) {
 
   $accessToken = $tkn->access_token;
   $_SESSION['refreshToken'] = $tkn->refresh_token;
-
+  $response['access_token'] = $accessToken;
+  $json = http_get($iotAppApiBaseUrl."/v1/?op=list&accessToken=" . $accessToken);
+  if ($json['httpcode'] == 200) {
+    $response['applications'] = array();
+    foreach ($json['result'] as $app) {
+      $a = $app;
+      if($a['type']!='edge') {
+        if($a['type']=='plumber') {
+          $a['icon'] = 'dataAnalyticIcon.png';
+          @$a['iotapps'] = join(',', $a['iotapps']);
+        }
+        else if($a['type']=='portia') {
+          $a['icon'] = 'portiaIcon.png';
+        }
+        else
+          $a['icon'] = $a['type']=='basic' ? 'iotAppBasicIcon.png' : 'iotAppAdvIcon.png';
+      } else {
+        $os = explode('_', $a['edgetype']);
+        $os = $os[0];
+        if($os==='win32') 
+          $a['icon'] = 'iotAppBasicPcIcon.png';
+        else if($os=='android')
+          $a['icon'] = 'iotAppBasicMobileIcon.png';
+        else
+          $a['icon'] = 'iotAppBasicRaspberryIcon.png';
+      }
+      $a['dashboards'] = array();
+      //search for connected dashboards
+      $q = "SELECT DISTINCT id_dashboard as dashboardId,title_header as dashboardName, user as dashboardAuthor FROM Dashboard.Config_widget_dashboard w JOIN Dashboard.Config_dashboard d ON d.Id=w.id_dashboard WHERE appId='$app[id]' AND d.deleted='no'";
+      $r = mysqli_query($link, $q);
+      if($r)
+      {
+          while($row = mysqli_fetch_assoc($r))
+          {
+              array_push($a['dashboards'], $row);
+          }         
+      }
+      $response['applications'][] = $a;
+    }
+    $response['detail'] = 'Ok';
+  } else {
+    $response['detail'] = 'Ko';
+    $response['error'] = $json['result'];
+  }
+  /*
   $json = http_get($ownershipApiBaseUrl."/v1/list/?type=AppID;DAAppID&accessToken=" . $accessToken);
   //echo json_encode($json);
 
@@ -60,7 +104,7 @@ if (isset($_SESSION['refreshToken'])) {
       } else {
         $os = explode('_', $a['edgetype']);
         $os = $os[0];
-        if(/*$os==='linux' ||*/ $os==='win32') 
+        if($os==='win32') 
           $a['icon'] = 'iotAppBasicPcIcon.png';
         else if($os=='android')
           $a['icon'] = 'iotAppBasicMobileIcon.png';
@@ -86,12 +130,12 @@ if (isset($_SESSION['refreshToken'])) {
   } else {
     $response['detail'] = 'Ko';
     $response['error'] = $json['result'];
-  }
+  }*/
 } else {
   $response['detail'] = 'Ko';
   $response['error'] = 'no refresh token';
 }
-
+$response['refresh_token'] = $_SESSION['refreshToken'];
 echo json_encode($response);
 
 function http_get($url) {
@@ -107,5 +151,5 @@ function http_get($url) {
   $result = file_get_contents($url, false, $context);
   //echo "result:$result\n";
   //var_dump($http_response_header);
-  return array("httpcode" => explode(" ", $http_response_header[0])[1], "result" => json_decode($result));
+  return array("httpcode" => explode(" ", $http_response_header[0])[1], "result" => json_decode($result,true));
 }
