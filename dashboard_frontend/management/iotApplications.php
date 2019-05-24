d547554b7524b6e1ec8c8896c05095d06f3b9fec
KindofBlue79 <kindofblue79@gmail.com>
2019-05-24 11:25:19 +0200

    24_05_2019

--- dashboard_frontend/management/iotApplications.php
+++ dashboard_frontend/management/iotApplications.php
@@ -23,7 +23,7 @@ if (!isset($_SESSION)) {
 $link = mysqli_connect($host, $username, $password);
 mysqli_select_db($link, $dbname);
 
-checkSession('Manager');
+checkSession('Manager',"ssoLogin.php?redirect=".urlencode($appUrl."/management/iotApplications.php"));
 ?>
 
 <!DOCTYPE html>
