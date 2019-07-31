6ef5a7498458491aaa7cf3cf9a5bc2f1ab7b7694
KindofBlue79 <kindofblue79@gmail.com>
2019-07-31 15:29:10 +0200

    31_07_2019

--- dashboard_frontend/controllers/getDashboardIotApps.php
+++ dashboard_frontend/controllers/getDashboardIotApps.php
@@ -23,85 +23,80 @@ error_reporting(E_ERROR | E_NOTICE);
 date_default_timezone_set('Europe/Rome');
 
 session_start();
+checkSession('Manager');
+
 $link = mysqli_connect($host, $username, $password);
 mysqli_select_db($link, $dbname);
 
 if(isset($_SESSION['loggedUsername'])) 
 {
-    $dashboardId = $_REQUEST['dashboardId'];
-    $q = "SELECT user FROM Dashboard.Config_dashboard WHERE Id = $dashboardId";
-    $r = mysqli_query($link, $q);
-        
-    if($r)
+    if(!checkDashboardId($link, $dashboardId)) {
+        eventLog("invalid request for getDashboardIotApps.php for dashboardId = $dashboardId user: ".$_SESSION['loggedUsername']);
+        exit;
+    }
+    //$dashboardAuthor = mysqli_fetch_assoc($r)['user'];
+
+    if(isset($_SESSION['refreshToken'])) 
     {
-        $dashboardAuthor = mysqli_fetch_assoc($r)['user'];
-        
-        if(isset($_SESSION['refreshToken'])) 
+        $oidc = new OpenIDConnectClient($ssoEndpoint, $ssoClientId, $ssoClientSecret);
+        $oidc->providerConfigParam(array('token_endpoint' => $ssoTokenEndpoint));
+
+        $tkn = $oidc->refreshToken($_SESSION['refreshToken']);
+
+        $accessToken = $tkn->access_token;
+        $_SESSION['refreshToken'] = $tkn->refresh_token;
+
+        $q1 = "SELECT distinct(NodeRedMetrics.appId) FROM NodeRedMetrics WHERE NodeRedMetrics.appId IS NOT NULL AND NodeRedMetrics.appId <> '' AND NodeRedMetrics.name IN(SELECT distinct(id_metric) FROM Dashboard.Config_widget_dashboard WHERE id_dashboard = '$dashboardId') " .
+              "UNION " .
+              "SELECT distinct(NodeRedInputs.appId) FROM NodeRedInputs WHERE NodeRedInputs.appId IS NOT NULL AND NodeRedInputs.appId <> '' AND NodeRedInputs.name IN(SELECT distinct(id_metric) FROM Dashboard.Config_widget_dashboard WHERE id_dashboard = '$dashboardId')";
+
+        $r1 = mysqli_query($link, $q1);
+
+        $appsFromQuery = 0;
+        $appsFromOwnership = 0;
+
+        if($r1)
         {
-            $oidc = new OpenIDConnectClient($ssoEndpoint, $ssoClientId, $ssoClientSecret);
-            $oidc->providerConfigParam(array('token_endpoint' => $ssoTokenEndpoint));
+            $appsFromQuery = mysqli_num_rows($r1);
+            $myFilteredApps = [];
 
-            $tkn = $oidc->refreshToken($_SESSION['refreshToken']);
-
-            $accessToken = $tkn->access_token;
-            $_SESSION['refreshToken'] = $tkn->refresh_token;
-            
-            $q1 = "SELECT distinct(NodeRedMetrics.appId) FROM NodeRedMetrics WHERE NodeRedMetrics.appId IS NOT NULL AND NodeRedMetrics.appId <> '' AND NodeRedMetrics.name IN(SELECT distinct(id_metric) FROM Dashboard.Config_widget_dashboard WHERE id_dashboard = $dashboardId) " .
-                  "UNION " .
-                  "SELECT distinct(NodeRedInputs.appId) FROM NodeRedInputs WHERE NodeRedInputs.appId IS NOT NULL AND NodeRedInputs.appId <> '' AND NodeRedInputs.name IN(SELECT distinct(id_metric) FROM Dashboard.Config_widget_dashboard WHERE id_dashboard = $dashboardId)";
-            
-            $r1 = mysqli_query($link, $q1);
-            
-            $appsFromQuery = 0;
-            $appsFromOwnership = 0;
-            
-            if($r1)
+            while($row = mysqli_fetch_assoc($r1))
             {
-                $appsFromQuery = mysqli_num_rows($r1);
-                $myFilteredApps = [];
-                
-                while($row = mysqli_fetch_assoc($r1))
+                $apiUrl = $ownershipApiBaseUrl . "/v1/list/?type=AppID&accessToken=" . $accessToken;
+
+                $options = array(
+                    'http' => array(
+                            'header'  => "Content-type: application/json\r\n",
+                            'method'  => 'GET',
+                            'timeout' => 30,
+                            'ignore_errors' => true
+                    )
+                );
+
+                $context  = stream_context_create($options);
+                $myAppsJson = file_get_contents($apiUrl, false, $context);
+                $myApps = json_decode($myAppsJson);
+
+                for($i = 0; $i < count($myApps); $i++) 
                 {
-                    $apiUrl = $ownershipApiBaseUrl . "/v1/list/?type=AppID&accessToken=" . $accessToken;
-
-                    $options = array(
-                        'http' => array(
-                                'header'  => "Content-type: application/json\r\n",
-                                'method'  => 'GET',
-                                'timeout' => 30,
-                                'ignore_errors' => true
-                        )
-                    );
-
-                    $context  = stream_context_create($options);
-                    $myAppsJson = file_get_contents($apiUrl, false, $context);
-                    $myApps = json_decode($myAppsJson);
-                    
-                    for($i = 0; $i < count($myApps); $i++) 
+                    if($myApps[$i]->elementId == $row['appId'])
                     {
-                        if($myApps[$i]->elementId == $row['appId'])
-                        {
-                            $myFilteredApps[$row['appId']] = array('url'=>$myApps[$i]->elementUrl,'name'=>$myApps[$i]->elementName);
-                            $appsFromOwnership++;
-                        }
-                    } 
-                }
-                
-                echo json_encode(["result" => "Ok", "appsFromQuery" => $appsFromQuery, "appsFromOwnership" => $appsFromOwnership, "applications" => $myFilteredApps, "sql" => $q1, "apiUrl" => $apiUrl, "myApps" => $myApps]);
+                        $myFilteredApps[$row['appId']] = array('url'=>$myApps[$i]->elementUrl,'name'=>$myApps[$i]->elementName);
+                        $appsFromOwnership++;
+                    }
+                } 
             }
-            else
-            {
-                echo json_encode(["result" => "q1 KO"]);
-            }
+
+            echo json_encode(["result" => "Ok", "appsFromQuery" => $appsFromQuery, "appsFromOwnership" => $appsFromOwnership, "applications" => $myFilteredApps, /*"sql" => $q1, "apiUrl" => $apiUrl,*/ "myApps" => $myApps]);
         }
         else
         {
-            echo json_encode(["result" => "token KO"]);
+            echo json_encode(["result" => "q1 KO"]);
         }
     }
     else
     {
-        echo json_encode(["result" => "q0 KO"]);
+        echo json_encode(["result" => "token KO"]);
     }
 }
   
