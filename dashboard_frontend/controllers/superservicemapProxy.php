010bdca5708ac361a41964896a5682cf4721b340
KindofBlue79 <kindofblue79@gmail.com>
2019-07-31 15:49:31 +0200

    31_07_2019

---
+++ dashboard_frontend/controllers/superservicemapProxy.php
@@ -0,0 +1,54 @@
+<?php
+
+/* Dashboard Builder.
+   Copyright (C) 2017 DISIT Lab https://www.disit.org - University of Florence
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA. */
+
+include '../config.php';
+require '../sso/autoload.php';
+use Jumbojett\OpenIDConnectClient;
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+if(isset($_SESSION['refreshToken'])) {
+    $oidc = new OpenIDConnectClient($ssoEndpoint, $ssoClientId, $ssoClientSecret);
+    $oidc->providerConfigParam(array('token_endpoint' => $ssoTokenEndpoint));
+    $tkn = $oidc->refreshToken($_SESSION['refreshToken']);
+    $accessToken = $tkn->access_token;
+    $_SESSION['refreshToken'] = $tkn->refresh_token;
+}
+session_write_close();
+$uri = $_SERVER['REQUEST_URI'];
+$p = strpos($uri,'superservicemapProxy.php');
+$x =  substr($uri, $p+strlen('superservicemapProxy.php'));
+if(substr($x,0,7)=='/api/v1') {
+  $apiUrl = "https://www.disit.org/superservicemap" . $x;
+  //$apiUrl = "https://www.disit.org/ServiceMap" . $x;
+
+  $options = array(
+      'http' => array(
+          'method' => 'GET',
+          'timeout' => 30,
+          'ignore_errors' => true
+      )
+  );
+  if(isset($accessToken)) {
+    $options['http']['header'] = "Authorization: Bearer $accessToken\r\n";
+  }
+  
+  $context = stream_context_create($options);
+  $result = file_get_contents($apiUrl, false, $context);
+  header("Content-Type: application/json");
+  echo $result;
+}
