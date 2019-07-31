f169661320fb9ce3ebd5d63c27cbad6ecee906f5
KindofBlue79 <kindofblue79@gmail.com>
2019-07-31 15:36:45 +0200

    31_07_2019

--- dashboard_frontend/widgets/widgetEvacuationPlansNew.php
+++ dashboard_frontend/widgets/widgetEvacuationPlansNew.php
@@ -28,6 +28,11 @@ header("Cache-Control: private, max-age=$cacheControlMaxAge");
         $replacements[0] = ' ';
         $replacements[1] = '&apos;';
         $title = $_REQUEST['title_w'];
+        $link = mysqli_connect($host, $username, $password);
+        if (checkWidgetNameInDashboard($link, $_REQUEST['name_w'], $_REQUEST['id_dashboard']) === false) {
+            eventLog("Returned the following ERROR in widgetEvacuationPlansNew.php for the widget ".escapeForHTML($_REQUEST['name_w'])." is not instantiated or allowed in this dashboard.");
+            exit();
+        }
         ?>
 
         var intervalRef, timeFontSize, scroller, widgetProperties, styleParameters, icon, serviceUri,
@@ -579,7 +584,6 @@ header("Cache-Control: private, max-age=$cacheControlMaxAge");
         }
 
         //addLink(widgetName, url, linkElement, divContainer);
-        //$("#<?= $_REQUEST['name_w'] ?>_titleDiv").html("<?= preg_replace($titlePatterns, $replacements, $title) ?>");
         //widgetProperties = getWidgetProperties(widgetName);
 
         //Nuova versione
