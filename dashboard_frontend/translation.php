c67df8874722682d6220069585c93fe3cbb5b679
Distributed Systems and Internet Technology Lab, Distributed Data Intelligence and Tech. Lab <info@disit.org>
2023-09-12 09:15:44 +0200

    12_09_2023

--- dashboard_frontend/translation.php
+++ dashboard_frontend/translation.php
@@ -1,45 +1,36 @@
 <?php
-function translate_string($string, $lenguage, $link) {
-include_once '../config.php';
-//require '../sso/autoload.php';
-//use Jumbojett\OpenIDConnectClient;
+/* Dashboard Builder.
+   Copyright (C) 2017 DISIT Lab https://www.disit.org - University of Florence
 
-
-//$link = mysqli_connect($host, $username, $password) or die("failed to connect to server !!");
-//mysqli_select_db($link, $dbname);
-//echo($dbname);
-//$test = translate_string('Settings', 'it_IT', $link);
-//$lenguage = 'Settings';
-//$string = 'it_IT';
-//echo($test);
-    $query = "SELECT translatedText FROM Dashboard.multilanguage WHERE language='" . $lenguage . "' AND menuText='" . $string . "';";
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Affero General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Affero General Public License for more details.
+   You should have received a copy of the GNU Affero General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+   
+function translate_string($string, $language, $link) {
+    $query = "SELECT translatedText FROM Dashboard.multilanguage WHERE language='" . $language . "' AND menuText='" . $string . "';";
     $result = mysqli_query($link, $query) or die(mysqli_error($link));
-    //
+
     $text = $string;
-    //print_r($result);
     $translatedText = "";
     if ($result) {
         $n = count($result);
         if ($n > 0){
-        while($row = mysqli_fetch_assoc($result)){
-            $translatedText = $row['translatedText'];
-            if (($translatedText !== null)&&($translatedText !== "")) {
-                $text = $translatedText;
-                //return($text);
-            } else {
-                
-               // return($text);
+            while($row = mysqli_fetch_assoc($result)){
+                $translatedText = $row['translatedText'];
+                if (($translatedText !== null)&&($translatedText !== "")) {
+                    $text = $translatedText;
+                } 
             }
         }
-        }else{
-            //return($text);  
-        }
-    } else {
-        //return($text);
     }
-    //
     return($text);
-   // echo($text);
 }
 
 //translate_string('Documentation and Articles', 'it_IT');
