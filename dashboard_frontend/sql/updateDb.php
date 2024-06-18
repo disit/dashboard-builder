35b24300aa0ee9da813f732f5fd637be01b4e478
Distributed Systems and Internet Technology Lab, Distributed Data Intelligence and Tech. Lab <info@disit.org>
2024-06-18 14:29:07 +0200

    fix_updateDb

--- dashboard_frontend/sql/updateDb.php
+++ dashboard_frontend/sql/updateDb.php
@@ -50,23 +50,30 @@ foreach ($tablesUpdateMap as $table => $columns) {
         continue;
     }
 
-    foreach ($columns as $column => $details) {
-        if (!isset($details['type'])) {
-            echo "Error: no data type has been specified for column '$column' in table '$table'".$NL;
-            continue;
-        }
+    // Check if $columns is an array of objects or a single object
+    if (!isset($columns[0])) {
+        $columns = [$columns];
+    }
 
-        $result = $conn->query("SHOW COLUMNS FROM `$table` LIKE '$column'");
-
-        if ($result->num_rows == 0) {
-            $queryCol = "ALTER TABLE `$table` ADD `$column` {$details['type']} {$details['extra']}";
-            if ($conn->query($queryCol) === TRUE) {
-                echo "The Column '$column' has been successfully added to Table '$table'".$NL;
-            } else {
-                echo "Error in adding column '$column' to table '$table': " . $conn->error . $NL;
+    foreach ($columns as $columnDetails) {
+        foreach ($columnDetails as $column => $details) {
+            if (!isset($details['type'])) {
+                echo "Error: no data type has been specified for column '$column' in table '$table'".$NL;
+                continue;
             }
-        } else {
-            echo "Table '$table' already has the column '$column'".$NL;
+
+            $result = $conn->query("SHOW COLUMNS FROM `$table` LIKE '$column'");
+
+            if ($result->num_rows == 0) {
+                $queryCol = "ALTER TABLE `$table` ADD `$column` {$details['type']} {$details['extra']}";
+                if ($conn->query($queryCol) === TRUE) {
+                    echo "The Column '$column' has been successfully added to Table '$table'".$NL;
+                } else {
+                    echo "Error in adding column '$column' to table '$table': " . $conn->error . $NL;
+                }
+            } else {
+                echo "Table '$table' already has the column '$column'".$NL;
+            }
         }
     }
 }
@@ -79,17 +86,24 @@ foreach ($tablesSchemaUpdateMap as $table => $columns) {
         continue;
     }
 
-    foreach ($columns as $column => $details) {
-        if (!isset($details['edit'])) {
-            echo "Error: no modifications have been specified for column '$column' in table '$table'".$NL;
-            continue;
-        }
+    // Check if $columns is an array of objects or a single object
+    if (!isset($columns[0])) {
+        $columns = [$columns];
+    }
 
-        $queryCol = "ALTER TABLE `$table` MODIFY `$column` {$details['edit']}";
-        if ($conn->query($queryCol) === TRUE) {
-            echo "The Column '$column' has been successfully modified in Table '$table'".$NL;
-        } else {
-            echo "Error in modifying column '$column' in table '$table': " . $conn->error . $NL;
+    foreach ($columns as $columnDetails) {
+        foreach ($columnDetails as $column => $details) {
+            if (!isset($details['edit'])) {
+                echo "Error: no modifications have been specified for column '$column' in table '$table'".$NL;
+                continue;
+            }
+
+            $queryCol = "ALTER TABLE `$table` MODIFY `$column` {$details['edit']}";
+            if ($conn->query($queryCol) === TRUE) {
+                echo "The Column '$column' has been successfully modified in Table '$table'".$NL;
+            } else {
+                echo "Error in modifying column '$column' in table '$table': " . $conn->error . $NL;
+            }
         }
     }
 }
@@ -101,36 +115,44 @@ foreach ($tableRecordUpdateMap as $table => $queries) {
         echo "Error: table '$table' does not exists in the Database.".$NL;
         continue;
     }
-    $column = $queries['column'];
-    $value = $queries['value'];
-    $updateQuery = $queries['update'];
-    $insertQuery = $queries['insert'];
 
-    if ($conn->query($updateQuery) === TRUE) {
-        echo "Update query info: " . $conn->info . $NL;  // Debug information
-        if (strpos($conn->info, 'Rows matched: 1') !== false) {
-            if (strpos($conn->info, 'Changed: 1') !== false) {
-                echo "Record in table '$table' has been successfully updated." . $NL;
-            } else if (strpos($conn->info, 'Changed: 0') !== false) {
-                echo "Record in table '$table' was already updated and has not been modified." . $NL;
-            }
-        } else {
-            // Check if the record already exists
-            $checkQuery = "SELECT * FROM `$table` WHERE `$column` = '$value'";
-            $checkResult = $conn->query($checkQuery);
+    // Check if $queries is an array of objects or a single object
+    if (!isset($queries[0])) {
+        $queries = [$queries];
+    }
 
-            if ($checkResult && $checkResult->num_rows == 0) {
-                if ($conn->query($insertQuery) === TRUE) {
-                    echo "Record has been successfully inserted into table '$table'.".$NL;
-                } else {
-                    echo "Error in inserting record into table '$table': " . $conn->error . $NL;
+    foreach ($queries as $queryDetails) {
+        $column = $queryDetails['column'];
+        $value = $queryDetails['value'];
+        $updateQuery = $queryDetails['update'];
+        $insertQuery = $queryDetails['insert'];
+
+        if ($conn->query($updateQuery) === TRUE) {
+            echo "Update query info: " . $conn->info . $NL;  // Debug information
+            if (strpos($conn->info, 'Rows matched: 1') !== false) {
+                if (strpos($conn->info, 'Changed: 1') !== false) {
+                    echo "Record in table '$table' has been successfully updated." . $NL;
+                } else if (strpos($conn->info, 'Changed: 0') !== false) {
+                    echo "Record in table '$table' was already updated and has not been modified." . $NL;
                 }
             } else {
-                echo "Record already exists in table '$table'.".$NL;
+                // Check if the record already exists
+                $checkQuery = "SELECT * FROM `$table` WHERE `$column` = '$value'";
+                $checkResult = $conn->query($checkQuery);
+
+                if ($checkResult && $checkResult->num_rows == 0) {
+                    if ($conn->query($insertQuery) === TRUE) {
+                        echo "Record has been successfully inserted into table '$table'.".$NL;
+                    } else {
+                        echo "Error in inserting record into table '$table': " . $conn->error . $NL;
+                    }
+                } else {
+                    echo "Record already exists in table '$table'.".$NL;
+                }
             }
+        } else {
+            echo "Error in updating record in table '$table': " . $conn->error . $NL;
         }
-    } else {
-        echo "Error in updating record in table '$table': " . $conn->error . $NL;
     }
 }
 
