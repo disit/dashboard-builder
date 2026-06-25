8f2c9692488329ac3ec5f19785df9270326e9037
Distributed Systems and Internet Technology Lab, Distributed Data Intelligence and Tech. Lab <info@disit.org>
2026-06-25 09:48:49 +0200

    configurable Privacy Policy and Cookies Policy links in new layout

--- dashboard_frontend/management/footer.php
+++ dashboard_frontend/management/footer.php
@@ -1,7 +1,32 @@
+<?php
+if (!function_exists('s4cMenuFooterIframeLink')) {
+    function s4cMenuFooterIframeLink($link, $pageTitle)
+    {
+        if ($link === '#' || stripos($link, 'iframeApp.php') !== false) {
+            return $link;
+        }
+
+        return 'iframeApp.php?linkUrl=' . urlencode($link) . '&pageTitle=' . urlencode($pageTitle) . '&fromSubmenu=false';
+    }
+}
+
+$menuPrivacyPolicyLink = (isset($privacyPolicyLink) && trim($privacyPolicyLink) !== '') ? trim($privacyPolicyLink) : '#';
+$menuPrivacyPolicyLinkName = (isset($privacyPolicyLinkName) && trim($privacyPolicyLinkName) !== '') ? trim($privacyPolicyLinkName) : 'Privacy Policy';
+$menuCookiesPolicyLink = (isset($cookiesPolicyLink) && trim($cookiesPolicyLink) !== '') ? trim($cookiesPolicyLink) : '#';
+$menuCookiesPolicyLinkName = (isset($cookiesPolicyLinkName) && trim($cookiesPolicyLinkName) !== '') ? trim($cookiesPolicyLinkName) : 'Cookies Policy';
+
+$menuPrivacyPolicyLink = s4cMenuFooterIframeLink($menuPrivacyPolicyLink, $menuPrivacyPolicyLinkName);
+$menuCookiesPolicyLink = s4cMenuFooterIframeLink($menuCookiesPolicyLink, $menuCookiesPolicyLinkName);
+
+$menuPrivacyPolicyLinkAttr = function_exists('s4cHtmlAttr') ? s4cHtmlAttr($menuPrivacyPolicyLink) : htmlspecialchars($menuPrivacyPolicyLink, ENT_QUOTES, 'UTF-8');
+$menuPrivacyPolicyLinkNameText = function_exists('s4cHtmlText') ? s4cHtmlText($menuPrivacyPolicyLinkName) : htmlspecialchars($menuPrivacyPolicyLinkName, ENT_QUOTES, 'UTF-8');
+$menuCookiesPolicyLinkAttr = function_exists('s4cHtmlAttr') ? s4cHtmlAttr($menuCookiesPolicyLink) : htmlspecialchars($menuCookiesPolicyLink, ENT_QUOTES, 'UTF-8');
+$menuCookiesPolicyLinkNameText = function_exists('s4cHtmlText') ? s4cHtmlText($menuCookiesPolicyLinkName) : htmlspecialchars($menuCookiesPolicyLinkName, ENT_QUOTES, 'UTF-8');
+?>
 <div id="footer">
 	<div class="footer-links">
-		<a href="https://www.snap4city.org/dashboardSmartCity/management/iframeApp.php?linkUrl=https://www.snap4city.org/drupal/node/49&pageTitle=Privacy%20Policy&fromSubmenu=false">Privacy Policy</a>
-		<a href="https://www.snap4city.org/dashboardSmartCity/management/iframeApp.php?linkUrl=https://www.snap4city.org/drupal/node/48&pageTitle=Cookies%20Policy&fromSubmenu=false">Cookies Policy</a>
+		<a href="<?php echo $menuPrivacyPolicyLinkAttr; ?>"><?php echo $menuPrivacyPolicyLinkNameText; ?></a>
+		<a href="<?php echo $menuCookiesPolicyLinkAttr; ?>"><?php echo $menuCookiesPolicyLinkNameText; ?></a>
 		<div class="theme-toggler">
 			
 			<select id="theme-switcher">
